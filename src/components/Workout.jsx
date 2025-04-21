73ac1120865f470f8a9309dab28c2c734eed0a5b
Fabian <128451994+fabs-pe@users.noreply.github.com>
2025-04-21 11:21:38 +1000

    file changes

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import SectionWrapper from './SectionWrapper'
+
 import ExcerciseCard from './ExcerciseCard'
+import SectionWrapper from './sectionWrapper'
 
 export default function Workout(props) {
     const { workout } = props
